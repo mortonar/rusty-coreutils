2b9eed2804d93751aebafb023c72df56a06ea371
Alex Morton <mortonar01@gmail.com>
2022-04-19 17:48:14 -0400

    initial -l support

--- src/ls/main.rs
+++ src/ls/main.rs
@@ -1,5 +1,9 @@
+use chrono::offset::Utc;
+use chrono::DateTime;
 use clap::Parser;
 use std::fs;
+use std::fs::DirEntry;
+use std::os::unix::fs::{MetadataExt, PermissionsExt};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -8,6 +12,9 @@ struct Cli {
     /// do not ignore entries starting with "."
     #[clap(short, long)]
     all: bool,
+    /// use a long listing format
+    #[clap(short)]
+    long: bool,
 }
 
 fn main() {
@@ -17,15 +24,43 @@ fn main() {
     }
     for f in cli.files {
         let paths = fs::read_dir(f).unwrap();
-        let mut paths: Vec<String> = paths
-            .map(|p| p.unwrap().file_name().into_string().unwrap())
-            .collect();
+        let mut entries: Vec<DirEntry> = paths.map(|p| p.unwrap()).collect();
 
         // TODO current and parent directory are not listed when "-a" is given
         if !cli.all {
-            paths.retain(|p| !p.starts_with('.'));
+            entries.retain(|p| !p.file_name().into_string().unwrap().starts_with('.'));
         }
 
-        println!("{}", paths.join(" "));
+        if cli.long {
+            entries.iter().for_each(|e| {
+                let metadata = e.metadata().unwrap();
+                let datetime: DateTime<Utc> = metadata.modified().unwrap().into();
+                // TODO align the columns on this
+                println!(
+                    "{} {} {} {} {} {} {}",
+                    unix_mode::to_string(metadata.permissions().mode()),
+                    metadata.nlink(),
+                    users::get_user_by_uid(metadata.uid())
+                        .unwrap()
+                        .name()
+                        .to_str()
+                        .unwrap(),
+                    users::get_group_by_gid(metadata.gid())
+                        .unwrap()
+                        .name()
+                        .to_str()
+                        .unwrap(),
+                    metadata.size(),
+                    datetime.format("%h %d %R"),
+                    e.file_name().into_string().unwrap()
+                )
+            });
+        } else {
+            let entries: Vec<String> = entries
+                .iter()
+                .map(|e| e.file_name().into_string().unwrap())
+                .collect();
+            println!("{}", entries.join(" "));
+        }
     }
 }
