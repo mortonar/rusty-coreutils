21f4ddede81904cf256f9a0071a3800ce8842376
Alex Morton <mortonar01@gmail.com>
2022-05-16 13:45:09 -0400

    Add -p and -v to mkdir

--- src/mkdir/main.rs
+++ src/mkdir/main.rs
@@ -7,6 +7,10 @@ use std::process::exit;
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     directories: Vec<String>,
+    #[clap(short, long)]
+    parents: bool,
+    #[clap(short, long)]
+    verbose: bool,
 }
 
 fn main() -> Result<()> {
@@ -18,10 +22,19 @@ fn main() -> Result<()> {
         exit(1);
     } else {
         for d in cli.directories {
-            if let Err(e) = fs::create_dir(&d) {
+            // TODO refactor (with decorator pattern?) so we don't have to keep checking this command line param (it won't change)
+            let create_res = if cli.parents {
+                fs::create_dir_all(&d)
+            } else {
+                fs::create_dir(&d)
+            };
+            if let Err(e) = create_res {
                 // TODO Truncate the "(os error #)" being reported on MacOS?
                 eprintln!("mkdir: failed to create directory '{}': {}", &d, e);
                 return_value = 1;
+            } else if cli.verbose {
+                // TODO this message is different in MacOS's version of mkdir
+                println!("mkdir: created directory {}", &d);
             }
         }
     }
